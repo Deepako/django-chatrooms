b5d60a66342f783c7d3ce8a7b33478f73c2444fd
Deepak N <deepak.narasimhan17@gmail.com>
2018-01-13 21:17:36 -0800

    2nd attempted ModuleNotFoundError fix

--- chatrooms/admin.py
+++ chatrooms/admin.py
@@ -1,8 +1,7 @@
 #encoding=utf8
 
 from django.contrib import admin
-from django.db import models
-from models import Room, Message
+from chatroom.models import Room, Message
 
 
 class RoomAdmin(admin.ModelAdmin):
